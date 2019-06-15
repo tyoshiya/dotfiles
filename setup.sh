81decf1e56eca8f5c9e356af756587d1aef5e556
吉屋翼 <tyoshiya.xeno.works@gmail.com>
2019-06-15 20:33:33 +0900

    no message

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-#カレント内のドットファイルの中でsetupignore_filesに記載のないもののシンボリックリンクを貼る
-for f in .??*
-do
-    s=true
-    while read line
-    do
-        [[ $f == $line ]] && s=false
-    done < setup_ignore_files
+#zshがインストールされているか確認してインストールされていなければインストールする
+result=`zsh --version`
+echo $result
 
-    [[ "$s" == false ]] && continue
-    ln -fs $HOME/dotfiles/$f $HOME/$f
-    echo "$HOME/dotfiles/$f linked!"
-done
+##カレント内のドットファイルの中でsetupignore_filesに記載のないもののシンボリックリンクを貼る
+#for f in .??*
+#do
+#    s=true
+#    while read line
+#    do
+#        [[ $f == $line ]] && s=false
+#    done < setup_ignore_files
+#
+#    [[ "$s" == false ]] && continue
+#    ln -fs $HOME/dotfiles/$f $HOME/$f
+#    echo "$HOME/dotfiles/$f linked!"
+#done
